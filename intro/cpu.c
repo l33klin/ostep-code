61885f4f4b4b24fb89d699b35688aaec321acb29
l33klin <l33klin@gmail.com>
2020-03-20 00:14:50 +0800

    add gitignore, remove unused code

--- intro/cpu.c
+++ intro/cpu.c
@@ -2,17 +2,16 @@
 #include <stdlib.h>
 #include "common.h"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     if (argc != 2) {
-	fprintf(stderr, "usage: cpu <string>\n");
-	exit(1);
+        fprintf(stderr, "usage: cpu <string>\n");
+        exit(1);
     }
     char *str = argv[1];
 
     while (1) {
-	printf("%s\n", str);
-	Spin(1);
+        printf("%s\n", str);
+        Spin(1);
     }
     return 0;
 }
